f72e0c35fde3251ce7a86a6873ef176e05703754
Wan Souza <wansoul@gmail.com>
2019-08-13 15:57:28 -0300

    Temp index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import CardsScroller from "../components/CardsScroller"
 
 const IndexPage = () => (
   <Layout>
@@ -11,7 +10,6 @@ const IndexPage = () => (
       Em breve muito conteúdo sobre uso de animações em inerfaces de usuário e
       como elas podem melhorar a experiência dos usuários com produtos digitais.
     </p>
-    <CardsScroller />
   </Layout>
 )
 
