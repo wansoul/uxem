e21b4aa98dabd5a389ed35a9569b214f132c7945
wansoul <wansoul@gmail.com>
2019-10-21 22:03:17 -0300

    Fixing subscription form

--- src/components/SubscriptionForm.js
+++ src/components/SubscriptionForm.js
@@ -1,16 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 
 import "./SubscriptionForm.css"
 
 const SubscriptionForm = () => {
+    const [email, setEmail] = useState(email);
     return (
         <div>
-            <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css" />
             <div id="mc_embed_signup">
             <form action="https://gmail.us20.list-manage.com/subscribe/post?u=713705bdc72a78c241e9e0ce0&amp;id=ebefa458dc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <div id="mc_embed_signup_scroll">
                 <label for="mce-EMAIL">Quero saber das novidades</label>
-                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required />
+                <input type="email" value={email} name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required />
+                
                 <div id="hidden-field" aria-hidden="true">
                     <input type="text" name="b_713705bdc72a78c241e9e0ce0_ebefa458dc" tabindex="-1" value="" />
                 </div>
